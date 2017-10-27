7d9fec7377097ce6081fed23f70bbc603aa9b88f
phoenixIS <savchira9@gmail.com>
2017-10-28 02:30:10 +0300

    Continuous random variables

---
+++ ISM/Iism_3/Iism_3/BaseDistribuionRule.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Iism_3
+{
+    public abstract class BaseDistribuionRule
+    {
+        public int N { get; }
+        public double[] ValuesArr
+        {
+            get
+            {
+                return (double[]) Values.Clone();
+            }
+        }
+        protected double[] Values { get; }
+        protected Random RandObj { get; }
+
+        private double? mathExpectation;
+        public double? MathExpectation
+        {
+            get
+            {
+                mathExpectation = mathExpectation ?? GetMathExpectation();
+                return mathExpectation;
+            }
+        }
+
+        private double? dispersion;
+        public double? Dispersion
+        {
+            get
+            {
+                dispersion = dispersion ?? GetDispersion();
+                return dispersion;
+            }
+        }
+
+        public double? TrueMathExpectation => GetTrueMathExpectation();
+        public double? TrueDispersion => GetTrueDispersion();
+
+
+        private double[] f;
+        public double[] F
+        {
+            get
+            {
+                f = f ?? CalculateDistributionFunctions();
+                return (double[]) f.Clone();
+            }
+        }
+
+        protected BaseDistribuionRule(int N)
+        {
+            this.N = N;
+            Values = new double[N];
+            RandObj = new Random();
+        }
+
+        protected abstract double? GetTrueMathExpectation();
+        protected abstract double? GetTrueDispersion();
+        public abstract double CalculateDistributionFunction(double x);
+
+        protected virtual double? GetMathExpectation()
+        {
+            double mathExpectation = 0.0;
+            for(int i = 0; i < N; i++)
+            {
+                mathExpectation += Values[i];
+            }
+            return mathExpectation / N;
+        }
+
+        protected virtual double? GetDispersion()
+        {
+            double dispersion = 0.0;
+            for(int i = 0; i < N; i++)
+            {
+                dispersion += Math.Pow(Values[i] - MathExpectation.Value, 2);
+            }
+            return dispersion / (N - 1);
+        }
+
+        public void Sort()
+        {
+            Array.Sort(Values);
+        }
+
+        private double[] CalculateDistributionFunctions()
+        {
+            double[] arr = new double[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                arr[i] = CalculateDistributionFunction(Values[i]);
+            }
+            return arr;
+        }
+    }
+}
---
+++ ISM/Iism_3/Iism_3/CauchyDistributionRule.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Iism_3
+{
+    public class CauchyDistributionRule : BaseDistribuionRule
+    {
+        private double m; //parameter of place
+        private double c; //parameter of scale
+
+        public CauchyDistributionRule(double m, double c, int N) : base(N)
+        {
+            this.m = m;
+            this.c = c;
+            GenerateValues();
+        }
+
+        private void GenerateValues()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                //double x = GetRandomInRange(-1, 1);
+                double y = GetRandomInRange(0, 1);
+
+                Values[i] = m + c * Math.Tan(Math.PI * (y - 0.5));
+            }
+        }
+
+        protected override double? GetTrueDispersion()
+        {
+            return null;
+        }
+
+        protected override double? GetTrueMathExpectation()
+        {
+            return null;
+        }
+
+        protected override double? GetDispersion()
+        {
+            return null;
+        }
+
+        protected override double? GetMathExpectation()
+        {
+            return null;
+        }
+
+        public override double CalculateDistributionFunction(double x)
+        {
+            return 0.5 + Math.Atan((x - m) / c) / Math.PI;
+        }
+
+        private double GetRandomInRange(double minimum, double maximum)
+        {
+            return RandObj.NextDouble() * (maximum - minimum) + minimum;
+        }
+    }
+}
